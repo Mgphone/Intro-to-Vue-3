03cde266e65b411c58040793eef1799c0b54f771
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-16 00:00:34 +0100

    first challenge

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const product = 'Socks'
+// const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return { product: "SOCKS", description: "sock is some kind of blah blah" };
+  },
+});
