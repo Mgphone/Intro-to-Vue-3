8f1a8dd1a9a5e152335d56ddbbb6a25d7002aa28
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-16 13:15:10 +0100

    need to add full http

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// if you creat only www.google.com not working
+// however if you did full http like http://www.google.com ok
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      // solution
+      url: "https://www.vuemastery.com/",
+      // solution
+      link: "http://www.google.com",
+    };
+  },
+});
