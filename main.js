98fd882b096163406e8a28245e072dd82979f76a
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-16 13:10:46 +0100

    l3-start-me

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      link: "/something",
+      alter: "This is sock",
+    };
+  },
+});
