500ef198feb6c6e578045d1112926ce66ee1cdbe
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-17 00:04:34 +0100

    using v-for with index and selected this index value and output using computed

--- main.js
+++ main.js
@@ -1,24 +1,56 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      brand: "Vue Mastery",
+      // image: './assets/images/socks_blue.jpg',
+      // inStock: false,
+      selectedVarient: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quality: 100,
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quality: 0,
+        },
+      ],
+      onSale: true,
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    // updateImage(variantImage) {
+    //   this.image = variantImage;
+    // },
+    updatedVariant(index) {
+      this.selectedVarient = index;
+      //   console.log(this.selectedVarient);
+    },
+  },
+  computed: {
+    title() {
+      return this.product + " " + this.brand;
+    },
+    image() {
+      return this.variants[this.selectedVarient].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVarient].quality;
+    },
+    sale() {
+      if (this.onSale) {
+        return "On Sale " + this.product + " " + this.brand;
+      }
+    },
+  },
+});
